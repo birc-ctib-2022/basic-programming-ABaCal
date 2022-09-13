35f9a5b6e7e2993b298e7ecd00177c54c7c2e15e
ABaCal <andresbccs@gmail.com>
2022-09-13 15:38:31 +0200

    Fixed

--- src/print-1-10.py
+++ src/print-1-10.py
@@ -1,6 +1,11 @@
 
 # Print the numbers described in the exercise
-i = 1 
+'''i = 1 
 while i <= 10:
     print(i)
-    i = i + 1
+    i += 1
+'''
+# Ahora con For
+
+for x in range(1,11):
+    print(x)
